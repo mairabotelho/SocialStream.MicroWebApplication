6e25da8c8ae2b5be2874350b2ce9a6140814b980
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-4.local>
2019-12-21 17:44:00 -0500

    video

--- src/main/java/com/zipcode/socialStream/controllers/VideoController.java
+++ src/main/java/com/zipcode/socialStream/controllers/VideoController.java
@@ -2,6 +2,7 @@ package com.zipcode.socialStream.controllers;
 
 import com.zipcode.socialStream.models.Video;
 import com.zipcode.socialStream.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,8 +12,10 @@ import javax.validation.Valid;
 
 @Controller
 public class VideoController {
+
         private VideoService videoService;
 
+        @Autowired
         public VideoController(VideoService videoService){
             this.videoService = videoService;
         }
@@ -23,9 +26,9 @@ public class VideoController {
 
         }
 
-        @GetMapping("/videos/{videoid}")
-        public @ResponseBody ResponseEntity<Video> show (@PathVariable Long videoID){
-            return new ResponseEntity<>(videoService.show(videoID), HttpStatus.OK);
+        @GetMapping("/videos/{videoId}")
+        public @ResponseBody ResponseEntity<Video> show (@PathVariable Long videoId){
+            return new ResponseEntity<>(videoService.show(videoId), HttpStatus.OK);
         }
 
         @PostMapping("/videos")
@@ -33,8 +36,8 @@ public class VideoController {
             return new ResponseEntity<>(videoService.create(video), HttpStatus.OK);
         }
 
-        @PutMapping("/videos")
-        public ResponseEntity<Video> update(Long videoId, @Valid @RequestBody Video video){
+        @PutMapping("/videos/{videoId}")
+        public ResponseEntity<Video> update(@PathVariable Long videoId, @Valid @RequestBody Video video){
             return new ResponseEntity<>(videoService.update(videoId, video), HttpStatus.OK);
         }
 
