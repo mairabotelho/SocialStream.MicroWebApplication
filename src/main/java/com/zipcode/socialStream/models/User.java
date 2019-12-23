4d838aa08ecdeca5789f30ae4988f467cae178f5
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-23 12:40:28 -0500

    added some fixes with the annotations

--- src/main/java/com/zipcode/socialStream/models/User.java
+++ src/main/java/com/zipcode/socialStream/models/User.java
@@ -1,14 +1,17 @@
 package com.zipcode.socialStream.models;
 
+import com.sun.tools.javah.Gen;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
 
     private String username;
