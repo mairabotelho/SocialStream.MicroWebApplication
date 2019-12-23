74872a4cc9e22c8f09c89ff1dca4766d7dfbdce5
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-23 09:38:02 -0500

    working on Comment and CommentTests

--- src/main/java/com/zipcode/socialStream/repositories/CommentRepository.java
+++ src/main/java/com/zipcode/socialStream/repositories/CommentRepository.java
@@ -3,5 +3,9 @@ package com.zipcode.socialStream.repositories;
 import com.zipcode.socialStream.models.Comment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    List<Comment> getAllByVideoId(Long videoId);
+    List<Comment> getAllByCommentId(Long commentId);
 }
