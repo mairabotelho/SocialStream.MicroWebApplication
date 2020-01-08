24ae2e959d9770d86f238aa437d8d2d59241c8cd
Amendoza20 <20.angel.mendoza@gmail.com>
2020-01-08 16:15:10 -0500

    Add cross origin whitelist

--- src/main/java/com/zipcode/socialStream/controllers/UserController.java
+++ src/main/java/com/zipcode/socialStream/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = { "http://localhost:4200", "https://cfapps.io", "https://social-stream-ui.cfapps.io", "https://social-stream-app.cfapps.io" })
 @Controller
 public class UserController {
 
