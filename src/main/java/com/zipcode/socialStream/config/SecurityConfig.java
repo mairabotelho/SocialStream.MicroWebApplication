8d2c1c793ee343e655b29eb550e4dc23779dd43e
Amendoza20 <20.angel.mendoza@gmail.com>
2020-01-09 16:08:29 -0500

    updated Security Config file

--- src/main/java/com/zipcode/socialStream/config/SecurityConfig.java
+++ src/main/java/com/zipcode/socialStream/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/signup", "/videos/**","/video-page/**","/videos0/**")
+                .antMatchers("/signup", "/videos/**","/video-page/**")
                 .permitAll()
                 .antMatchers("/login")
                 .permitAll()
